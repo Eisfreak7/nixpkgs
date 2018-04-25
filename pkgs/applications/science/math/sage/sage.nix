2aadd65a7a1c330ae8b620277b7d2bbe73ab6259
Timo Kaufmann <timokau@zoho.com>
2018-04-25 15:26:20 +0200

    sagemath: Test everything

--- pkgs/applications/science/math/sage/sage.nix
+++ pkgs/applications/science/math/sage/sage.nix
@@ -346,9 +346,7 @@ stdenv.mkDerivation rec {
       HOME="$sagehome" \
       SHELL="${stdenv.shell}" \
       TMP="$TMP" \
-      "$out/bin/sage" -t --nthreads "$NIX_BUILD_CORES" --timeout 0 --exitfirst --all
-      # TODO
-      #"$out/bin/sage" -t --nthreads "$NIX_BUILD_CORES" --timeout 0 --only-errors --exitfirst --long --all
+      "$out/bin/sage" -t --nthreads "$NIX_BUILD_CORES" --timeout 0 --long --all
   '';
   # TODO optionally enable sagedoc tests
 }
