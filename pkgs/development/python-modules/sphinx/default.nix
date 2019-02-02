24c3b7fcff58db2b60e180102e438e88381fcac4
Timo Kaufmann <timokau@zoho.com>
2019-02-02 11:26:54 +0100

    sphinx: 1.7.9 -> 1.8.3
    Sage (and its legacy notebook) need some attention to make it
    compatible.

--- pkgs/development/python-modules/sphinx/default.nix
+++ pkgs/development/python-modules/sphinx/default.nix
@@ -26,11 +26,11 @@
 
 buildPythonPackage rec {
   pname = "sphinx";
-  version = "1.7.9";
+  version = "1.8.3";
   src = fetchPypi {
     pname = "Sphinx";
     inherit version;
-    sha256 = "217a7705adcb573da5bbe1e0f5cab4fa0bd89fd9342c9159121746f593c2d5a4";
+    sha256 = "1y5ry56rsmncd0n8aph6sg51bqn1mrmnnr0968ysxzxc8jx1gjy4";
   };
   LC_ALL = "en_US.UTF-8";
 
