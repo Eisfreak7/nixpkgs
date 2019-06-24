6cf4b7cb4c5b0a423cd461863d1836291d7c2125
Timo Kaufmann <timokau@zoho.com>
2019-06-24 23:37:40 +0200

    tf: add nccl

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5367,6 +5367,7 @@ in {
       inherit (pkgs.linuxPackages) nvidia_x11;
       cudatoolkit = pkgs.cudatoolkit_10_0;
       cudnn = pkgs.cudnn_cudatoolkit_10_0;
+      nccl = pkgs.nccl_cudatoolkit_10_0;
       protobuf_cc = pkgs.protobuf; # not the python version
     };
 
